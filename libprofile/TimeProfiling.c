9b7243e5deb16520d815fcc763458cb6bab99f08
Meng Hao <haomeng1@yeah.net>
2015-12-25 21:00:03 +0800

    Add the time profiling to get time of communication

---
+++ libprofile/TimeProfiling.c
@@ -0,0 +1,19 @@
+#include "Profiling.h"
+#include <stdlib.h>
+
+static double *ArrayStart;
+static uint64_t NumElements;
+
+static void TimeProfAtExitHandler(void) {
+  write_profiling_data_double(BlockInfoDouble, ArrayStart, NumElements);
+}
+
+int llvm_start_time_profiling(int argc, const char** argv,
+                                    double* arrayStart, uint64_t numElements)
+{
+  int Ret = save_arguments(argc, argv);
+  ArrayStart = arrayStart;
+  NumElements = numElements;
+  atexit(TimeProfAtExitHandler);
+  return Ret;
+}
